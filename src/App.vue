dfffe8be86af0f246ee22abb990a174d09f54469
medmin <erichui329@gmail.com>
2018-10-30 20:34:37 -0700

    get rid of sweetalert2 to help Google verification

--- src/App.vue
+++ src/App.vue
@@ -25,33 +25,33 @@
       Ribbon,
       Header,
       Footer
-    },
-    created() {
-      if (!this.isLoggedIn) {
-        const swalWithBootstrapButtons = swal.mixin({
-          confirmButtonClass: "btn btn-success",
-          cancelButtonClass: "btn btn-danger",
-          buttonsStyling: true
-        });
-
-        swalWithBootstrapButtons({
-          title: "Are you over 18 years old?",
-          text: "Please advise.",
-          type: "question",
-          showCancelButton: true,
-          confirmButtonText: "Yes",
-          cancelButtonText: "No",
-          reverseButtons: true,
-          allowOutsideClick: false
-        }).then(result => {
-          if (result.value) {
-            return;
-          } else if (result.dismiss === swal.DismissReason.cancel) {
-            window.location.href = "https://google.com";
-          }
-        });
-      }
     }
+    // created() {
+    //   if (!this.isLoggedIn) {
+    //     const swalWithBootstrapButtons = swal.mixin({
+    //       confirmButtonClass: "btn btn-success",
+    //       cancelButtonClass: "btn btn-danger",
+    //       buttonsStyling: true
+    //     });
+
+    //     swalWithBootstrapButtons({
+    //       title: "Are you over 18 years old?",
+    //       text: "Please advise.",
+    //       type: "question",
+    //       showCancelButton: true,
+    //       confirmButtonText: "Yes",
+    //       cancelButtonText: "No",
+    //       reverseButtons: true,
+    //       allowOutsideClick: false
+    //     }).then(result => {
+    //       if (result.value) {
+    //         return;
+    //       } else if (result.dismiss === swal.DismissReason.cancel) {
+    //         window.location.href = "https://google.com";
+    //       }
+    //     });
+    //   }
+    // }
   };
 </script>
 

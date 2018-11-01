ff45f75118db583f39fcc2f4cceb516076f5c882
medmin <erichui329@gmail.com>
2018-10-31 20:07:46 -0700

    youtube api to process.env

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@
   import Ribbon from "./components/layout/GithubRibbon";
   import Header from "./components/layout/Header";
   import Footer from "./components/layout/Footer";
-  import swal from "sweetalert2";
 
   export default {
     name: "app",
@@ -26,32 +25,6 @@
       Header,
       Footer
     }
-    // created() {
-    //   if (!this.isLoggedIn) {
-    //     const swalWithBootstrapButtons = swal.mixin({
-    //       confirmButtonClass: "btn btn-success",
-    //       cancelButtonClass: "btn btn-danger",
-    //       buttonsStyling: true
-    //     });
-
-    //     swalWithBootstrapButtons({
-    //       title: "Are you over 18 years old?",
-    //       text: "Please advise.",
-    //       type: "question",
-    //       showCancelButton: true,
-    //       confirmButtonText: "Yes",
-    //       cancelButtonText: "No",
-    //       reverseButtons: true,
-    //       allowOutsideClick: false
-    //     }).then(result => {
-    //       if (result.value) {
-    //         return;
-    //       } else if (result.dismiss === swal.DismissReason.cancel) {
-    //         window.location.href = "https://google.com";
-    //       }
-    //     });
-    //   }
-    // }
   };
 </script>
 

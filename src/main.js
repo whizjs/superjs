55dd7ca0bfb97bded65c5dd4c8735949ee71a5ca
medmin <erichui329@gmail.com>
2018-11-22 16:26:35 -0800

    test netlify process node env

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import store from './store';
 import BootstrapVue from 'bootstrap-vue'
 import VueAnalytics from 'vue-analytics'
 
+const isProd = process.env.NODE_ENV === 'production';
+
 Vue.use(BootstrapVue);
 Vue.use(VueAnalytics, {
   id: 'UA-114773079-3',
   router
 })
+
+console.log(isProd);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'

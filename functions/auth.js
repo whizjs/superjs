47c580f2673ae8a029e5b82a7e6e311aa489b483
Yumin Gui <guiyumin@outlook.com>
2018-12-30 15:53:17 -0800

    test google oauth 6

--- functions/auth.js
+++ functions/auth.js
@@ -1,13 +1,30 @@
-import google from './auth/google';
+var Request = require('request');
 
-exports.handler = function (event, context, callback) {
-    let pathArray = event.path.split('/');
-    let subPath = pathArray[4] || pathArray[3];
-    switch (subPath) {
-        case 'google':
-            google(event, context, callback)
-            break;
-        default:
-            callback('404 Path Not Found!');
+Request({
+    method: 'post',
+    url: 'https://accounts.google.com/o/oauth2/token',
+    form: {
+        // code: req.body.code,
+        client_id: "820664319776-kratepkqp0lnq1kgsk6ob34pc7u3meug.apps.googleusercontent.com",
+        client_secret: "aBDbuxOOAFYJN3q5CdXpvj7_",
+        redirect_uri: "http://localhost:8080/oauth/google",
+        grant_type: 'authorization_code'
+    },
+    headers: {
+        'Allow-Control-Allow-Origin': "*",
+        'content-type': 'application/x-www-form-urlencoded'
     }
-}
+}, function (err, response, body) {
+    try {
+        if (!err && response.statusCode === 200) {
+            callback(null, {
+                statusCode: 200,
+                body: body
+            });
+        } else {
+            callback('error' + JSON.stringify(err));
+        }
+    } catch (e) {
+        callback('error' + JSON.stringify(e));
+    }
+})

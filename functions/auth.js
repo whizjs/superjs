77368b5460a18faf4b0daeea0faf66b080b81fb8
Yumin Gui <guiyumin@outlook.com>
2018-12-30 15:56:23 -0800

    test google oauth 7

--- functions/auth.js
+++ functions/auth.js
@@ -1,30 +1,31 @@
 var Request = require('request');
-
-Request({
-    method: 'post',
-    url: 'https://accounts.google.com/o/oauth2/token',
-    form: {
-        // code: req.body.code,
-        client_id: "820664319776-kratepkqp0lnq1kgsk6ob34pc7u3meug.apps.googleusercontent.com",
-        client_secret: "aBDbuxOOAFYJN3q5CdXpvj7_",
-        redirect_uri: "http://localhost:8080/oauth/google",
-        grant_type: 'authorization_code'
-    },
-    headers: {
-        'Allow-Control-Allow-Origin': "*",
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-}, function (err, response, body) {
-    try {
-        if (!err && response.statusCode === 200) {
-            callback(null, {
-                statusCode: 200,
-                body: body
-            });
-        } else {
-            callback('error' + JSON.stringify(err));
+exports.handler = function (event, context, callback) {
+    Request({
+        method: 'post',
+        url: 'https://accounts.google.com/o/oauth2/token',
+        form: {
+            // code: req.body.code,
+            client_id: "820664319776-kratepkqp0lnq1kgsk6ob34pc7u3meug.apps.googleusercontent.com",
+            client_secret: "aBDbuxOOAFYJN3q5CdXpvj7_",
+            redirect_uri: "http://localhost:8080/oauth/google",
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Allow-Control-Allow-Origin': "*",
+            'content-type': 'application/x-www-form-urlencoded'
         }
-    } catch (e) {
-        callback('error' + JSON.stringify(e));
-    }
-})
+    }, function (err, response, body) {
+        try {
+            if (!err && response.statusCode === 200) {
+                callback(null, {
+                    statusCode: 200,
+                    body: body
+                });
+            } else {
+                callback('error' + JSON.stringify(err));
+            }
+        } catch (e) {
+            callback('error' + JSON.stringify(e));
+        }
+    })
+}

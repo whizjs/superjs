ef798a9aa8865ec281605aa4a7d6c775fb148ca7
Yumin Gui <guiyumin@outlook.com>
2019-02-14 17:59:34 -0800

    random number to url

--- functions/auth.js
+++ functions/auth.js
@@ -13,7 +13,7 @@ exports.handler = function (event, context, callback) {
     const oauth2Client = new OAuth2Client(googleAppId, googleAppKey, googleCallback)
     if (parsedPathArr[parsedPathArr.length - 1] === 'callback') {
       oauth2Client.getToken(event.queryStringParameters.code).then((res) => {
-        const profileUrl = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json';
+        const profileUrl = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&ts=' + Date.now();
         // res.tokens 需要保存在服务器，不能返回给用户
         oauth2Client.setCredentials(res.tokens);
         oauth2Client.request({

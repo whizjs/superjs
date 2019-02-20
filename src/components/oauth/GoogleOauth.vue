560f265c7729cfb070a5f50b155e44ecab3b5b23
Yumin Gui <erichui329@gmail.com>
2019-02-19 19:47:19 -0800

    no redirect

--- src/components/oauth/GoogleOauth.vue
+++ src/components/oauth/GoogleOauth.vue
@@ -9,7 +9,7 @@
     name: "GoogleOauth",
     mounted() {
       axios
-        .get(`https://superjs.org/api/auth/google/callback${location.search}`)
+        .get(`/.netlify/functions/google-auth${location.search}`)
         .then(res => {
           const event = new Event("google-oauth");
           event.user = res.data;
